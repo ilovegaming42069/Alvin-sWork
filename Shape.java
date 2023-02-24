577a0fdfe504375c3e8b7720125d588b9fd2ef7a
ilovegaming42069 <114389460+ilovegaming42069@users.noreply.github.com>
2023-02-24 14:39:49 +0700

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,33 @@
+abstract class Shape {
+    String color;
+    abstract int getArea();
+    public Shape(String color){
+        this.color = color;
+    }
+
+}
+
+class Rectangle extends Shape {
+    int width;
+    int height;
+    public Rectangle(int width, int height, String color){
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+    @Override
+    public int getArea(){
+        return this.height*this.width;
+    }
+}
+
+class Gay {
+    public static void main(String[] args){
+        Rectangle constructor = new Rectangle(60, 60, "Gay");
+        System.out.print(constructor.getArea());
+    }
+}
+
+
+
+
